7d9b365cfd9702aaa90510ead5d6733da4c7de2c
othmanhanif <128992319+othmanhanif@users.noreply.github.com>
2025-06-13 10:32:52 +0700

    Update LineChart.jsx

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
-const LineChartcomponent = ({ data }) => {
+const LineChartcomponent = ({ data = [] }) => {
   return (
     <div style={{ width: '100%', height: 300 }}>
       <ResponsiveContainer>
-        <LineChartcomponent data={data}>
+        <LineChart data={data}>
           <XAxis dataKey="bulan" />
           <YAxis />
           <Tooltip />
           <Line type="monotone" dataKey="kerusakan" stroke="#007bff" />
-        </LineChartcomponent>
+        </LineChart>
       </ResponsiveContainer>
     </div>
   )
