1c4075b975af87d705bdcc345c850ecdc8e8d7fb
syhrll11 <panjigunawan327@gmail.com>
2025-06-22 07:41:10 +0700

    revisi laporan

--- src/_services/exportService.js
+++ src/_services/exportService.js
@@ -1,17 +1,18 @@
-// src/_services/exportService.js
 import * as XLSX from 'xlsx';
 import jsPDF from 'jspdf';
-import 'jspdf-autotable';
+import autoTable from 'jspdf-autotable';
 
 export const exportToExcel = (data, status) => {
-  const worksheet = XLSX.utils.json_to_sheet(data.map(item => ({
-    'Nama Aset': item.name_asset,
-    'Kode Gudang': item.kd_cabang,
-    'Tipe': item.kat_aset,
-    'Serial Number': item.serial_number,
-    'Status': item.trx_status.toUpperCase(),
-    'Tanggal': item.tanggal_keluar?.split('T')[0] || '-'
-  })));
+  const worksheet = XLSX.utils.json_to_sheet(
+    data.map(item => ({
+      'Nama Aset': item.name_asset,
+      'Kode Gudang': item.kd_cabang,
+      'Tipe': item.kat_aset,
+      'Serial Number': item.serial_number,
+      'Status': item.trx_status.toUpperCase(),
+      'Tanggal': item.tanggal_keluar?.split('T')[0] || '-',
+    }))
+  );
 
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, 'Data Aset');
@@ -20,6 +21,7 @@ export const exportToExcel = (data, status) => {
 
 export const exportToPDF = (data, status) => {
   const doc = new jsPDF();
+
   doc.text(`Laporan Aset (${status.toUpperCase()})`, 14, 10);
 
   const tableData = data.map(item => [
@@ -28,10 +30,10 @@ export const exportToPDF = (data, status) => {
     item.tipe_asset,
     item.serial_number,
     item.trx_status.toUpperCase(),
-    item.tanggal_keluar?.split('T')[0] || '-'
+    item.tanggal_keluar?.split('T')[0] || '-',
   ]);
 
-  doc.autoTable({
+  autoTable(doc, { 
     head: [['Nama Aset', 'Kode Gudang', 'Tipe', 'Serial Number', 'Status', 'Tanggal']],
     body: tableData,
     startY: 20,
